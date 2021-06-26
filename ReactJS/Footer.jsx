2eefa784a633035f5314cb13e32fb025ae05c18e
HariboobaalanPN <79743324+HariboobaalanPN@users.noreply.github.com>
2021-06-26 19:02:57 +0530

    Add files via upload

---
+++ ReactJS/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by ShapeAI React JS BootCamp@{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ ReactJS/Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>Basic Web development Using React JS</p>
+    </div>
+  );
+}
+export default Info;
